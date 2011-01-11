682736aa208a47914da94f4050c1f33239e39095
guns <sung@metablu.com>
2011-01-11 02:33:45 -0600

    First simple working implementation

--- application.rb
+++ application.rb
@@ -7,8 +7,65 @@
 #
 #                                             guns <sung@metablu.com>
 
+require 'fileutils'
+require 'shellwords'
+
 class VimServer
+  class VimError < StandardError; end
+
+  attr_accessor :request, :response
+
+  def vim *args
+    @@vim ||= begin
+      bin = %x(which vim).chomp
+      raise VimError, 'Vim not found!' unless File.executable? bin
+      bin
+    end
+
+    cmd = [@@vim, *args]
+    out = %x(#{cmd.shelljoin} 2>&1).chomp
+
+    if $?.exitstatus.zero?
+      out
+    else
+      raise VimError, out
+    end
+  end
+
+  def server_available?
+    vim('--serverlist').split("\n").map { |l| l.strip.downcase }.include? 'vimserver'
+  end
+
+  # FIXME: files should be temporary; client should send extant text
+  def edit name
+    if server_available?
+      file = "/tmp/vimpreview/#{name}"
+      FileUtils.mkdir_p File.dirname(file), :mode => 0700
+      File.open(file, 'a', 0600).close
+      vim '--servername', 'vimserver', '--remote-tab-wait', file
+      File.read file
+    else
+      raise VimError, 'No Vim server available!'
+    end
+  end
+
+  def filename
+    case request.env['HTTP_ORIGIN']
+    when 'chrome-extension://ppoadiihggafnhokfkpphojggcdigllp' # TextAid
+      request.params['id'] + ' ' + request.params['url'].gsub(/\W+/, '-') + '.txt'
+    else
+      'vimserver.txt'
+    end
+  end
+
   def call env
-    [200, { 'Content-Type' => 'text/plain' }, 'VimServer']
+    self.request  = Rack::Request.new env
+    self.response = Rack::Response.new
+
+    response.write edit(filename)
+    response.finish
+  rescue VimError => e
+    response.write e.to_s
+    response.finish
   end
 end
--- config.ru
+++ config.ru
@@ -1,8 +1,8 @@
 require 'bundler/setup'
 require File.expand_path('../application.rb', __FILE__)
 
-[$stdout, $stderr].each { |io| io.reopen 'vimserver.log', 'w' }
+[$stdout, $stderr].each { |io| io.reopen 'vimserver.log', 'a' }
 
 use Rack::Reloader, 1 unless ENV['RACK_ENV'] == 'production'
-use Rack::Logger
+use Rack::CommonLogger
 run VimServer.new
